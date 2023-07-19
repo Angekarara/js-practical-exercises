e23a3ac0d92c8c508ff5adea148a069ccc909c47
Angekarara <mbabange2020@gmail.com>
2023-07-19 13:47:07 +0200

    second question

---
+++ gcd.js
@@ -0,0 +1,16 @@
+function gcd(x,y){
+    if(typeof(x)!=='number'||typeof(y)!=='number' )
+    return false;
+}
+Math.abs(x);
+Math.abs(y);
+
+while(y>0){
+    let t = y;
+    y=x%y;
+    x=z;
+   console.log(x) ;
+
+}
+gcd(4,5)
+

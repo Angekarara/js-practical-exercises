ccf19042530e6120848c5168bd67e156ba173970
Angekarara <mbabange2020@gmail.com>
2024-05-30 18:37:59 +0200

    added Most frequently used words in a text

---
+++ usedWorlds.js
@@ -0,0 +1,24 @@
+function topThreeWords(text) {
+    let words = text.match(/\b[a-zA-Z']*[a-zA-Z][a-zA-Z']*\b/g);
+
+    if (!words) {
+        return [];
+    }
+
+    words = words.map(word => word.toLowerCase());
+
+    let occurrences = {};
+    words.forEach(word => {
+        if (occurrences[word]) {
+            occurrences[word]++;
+        } else {
+            occurrences[word] = 1;
+        }
+    });
+  const arr = Object.entries(occurrences)
+  const sort = arr.sort((a,b) => b[1] - a[1]).slice(0,3)
+   const s = Object.fromEntries(sort)
+  
+    return Object.keys(s);
+}
+console.log(topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"))

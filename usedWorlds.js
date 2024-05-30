7b0b2fe32640277d8a7b40a0b07e38cc03b35a59
Angekarara <mbabange2020@gmail.com>
2024-05-30 18:41:08 +0200

    added Most frequently used words in a text kata

--- usedWorlds.js
+++ usedWorlds.js
@@ -21,4 +21,6 @@ function topThreeWords(text) {
   
     return Object.keys(s);
 }
-console.log(topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"))
+console.log(topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"))//['e','ddd','aa']
+console.log(topThreeWords("a a a  b  c c  d d d d  e e e e e"))//['e','d','a']
+console.log(topThreeWords("  //wont won't won't "))// ["won't", "wont"]
